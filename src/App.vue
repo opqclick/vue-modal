8b4fac3a577cfabff51751ecf822f2c46ad50927
Opu Hasnat <opqclick@gmail.com>
2022-06-08 03:10:21 +0600

    click event modifiers

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" :theme="theme" @close="toggleModal" />
+  </div>
+  <button class="toggle_modal" @click.alt="toggleModal">
+    Show Modal (alt)
+  </button>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Modal from "./components/Modal.vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { Modal },
+  data() {
+    return {
+      title: "My first Vue App :)",
+      header: "Header prop data from app vue",
+      text: "Modal content from app vue via props",
+      theme: "sale",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
   },
 };
 </script>
 
-<style>
+<style scope>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +39,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+.toggle_modal {
+  display: block;
+  margin: 10px auto;
 }
 </style>
